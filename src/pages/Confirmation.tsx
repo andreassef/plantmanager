2c0d4bb2a623842f17225c3135928459b9678ffa
andreassef <andreassef02@gmail.com>
2021-04-20 14:40:35 -0400

    [UPDATE] - Adicao de atributo no button

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -21,7 +21,7 @@ export function Confirmation() {
                      plantinhas com muito cuidado
                 </Text>
                 <View style={styles.footer}>
-                    <Button />
+                    <Button title="Começar" />
                 </View>
             </View>
         </SafeAreaView>
