b107b43e18f99c79d57eaa0060f65e8bb3f348c0
andreassef <andreassef02@gmail.com>
2021-04-20 14:41:47 -0400

    [UPDATE] - Adicao de rotas no arquivo de entrada

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { Welcome } from './src/pages/Welcome';
+import { UserIdentification } from './src/pages/UserIdentification';
+import {Confirmation} from './src/pages/Confirmation';
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost'
+
+import Routes from './src/routes';
 
 export default function App() {
   const [ fontsLoaded] = useFonts({
@@ -18,7 +22,7 @@ export default function App() {
       <AppLoading />
     )
   return (
-    <Welcome />
+    <Routes />
   );
 }
 
