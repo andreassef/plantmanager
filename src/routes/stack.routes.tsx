ebd52eab2e2cff72fd74b61024063ede9aa5c675
andreassef <andreassef02@gmail.com>
2021-04-23 01:15:13 -0400

    [ADD] - Nova screen para detalhes da planta

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,6 +5,7 @@ import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
 import { PlantSelect } from '../pages/PlantSelect';
+import { PlantSave } from '../pages/PlantSave';
 
 const stackRoutes = createStackNavigator();
 
@@ -30,11 +31,16 @@ const AppRoutes: React.FC = () => (
             component={Confirmation}
         />
 
-        <stackRoutes.Screen 
+        <stackRoutes.Screen
             name="PlantSelect"
             component={PlantSelect}
-        /> 
-        
+        />
+
+        <stackRoutes.Screen
+            name="PlantSave"
+            component={PlantSave}
+        />
+
     </stackRoutes.Navigator>
 )
 
