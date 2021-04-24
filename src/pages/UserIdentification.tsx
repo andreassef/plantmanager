4670bc44bac86ea8861246601137b5e330e2d79c
andreassef <andreassef02@gmail.com>
2021-04-24 04:00:53 -0400

    [UPDATE] - Novas informações no navigate

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -30,8 +30,14 @@ export function UserIdentification() {
             return Alert.alert('Como podemos chama-lo? 🧐');
         
         try {
-            navigation.navigate('Confirmation');
             await AsyncStorage.setItem('@plantmanager:user', name);
+            navigation.navigate('Confirmation', {
+                title: 'Prontinho',
+                subtitle: 'Agora vamos começar a cuidar das suas plantinhas com muito cuidado.',
+                buttonTitle: 'Começar',
+                icon: 'smile',
+                nextScreen: 'PlantSelect'
+            });
         }catch {
             Alert.alert('Não foi possível salvar o nome do usuário. 🧐');
         }    
