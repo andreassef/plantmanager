2c7a1cdda95809b0d75fee6ece6f00cd4017a2dc
andreassef <andreassef02@gmail.com>
2021-04-25 17:24:42 -0400

    [ADD] - Adicionamos alguns listeners para ouvir as notificacoes

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppLoading from 'expo-app-loading';
-import { Welcome } from './src/pages/Welcome';
-import { UserIdentification } from './src/pages/UserIdentification';
-import {Confirmation} from './src/pages/Confirmation';
+
+import * as Notifications from 'expo-notifications';
+
 import {
   useFonts,
   Jost_400Regular,
@@ -10,12 +10,32 @@ import {
 } from '@expo-google-fonts/jost'
 
 import Routes from './src/routes';
+import { PlantProps } from './src/libs/storage';
 
 export default function App() {
   const [ fontsLoaded] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold
-  })
+  });
+
+  useEffect(() => {
+    // const subscription = Notifications.addNotificationReceivedListener(
+    //   async notification => {
+    //     const data = notification.request.content.data.plant as PlantProps;
+    //     console.log(data);
+    //   }
+    // );
+
+    // return () => subscription.remove();
+
+    async function notifications() {
+      const data = await Notifications.getAllScheduledNotificationsAsync();
+      console.log(data)
+    }
+
+    console.log('Notificações agendadas')
+    notifications();
+  }, [])
 
   if(!fontsLoaded)
     return (
